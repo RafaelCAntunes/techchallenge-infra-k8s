07a6e31a20a9c6244ad7b7006cc16168899765c6
Rafael Antunes <rafaelcantunes3@gmail.com>
2025-10-06 20:15:19 -0300

    ajuste na referencia da subnet privada para o node

--- main.tf
+++ main.tf
@@ -3,11 +3,10 @@ module "vpc" {
 }
 
 module "eks" {
-  source = "./eks"
-
+  source       = "./eks"
   cluster_name = var.cluster_name
   vpc_id       = module.vpc.vpc_id
-  subnet_ids   = module.vpc.subnet_ids
+  subnet_ids   = module.vpc.private_subnet_ids  # ← vamos criar esse output
   eks_role_arn  = module.iam.cluster_role_arn
   node_role_arn = module.iam.node_role_arn
 }
