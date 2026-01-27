3d78c5290717a468154903d086c9632eb82a7917
Rafael Antunes <rafaelcantunes3@gmail.com>
2026-01-26 23:38:35 -0300

    adiciando policy para permitir acesso ao dynamo

--- iam/outputs.tf
+++ iam/outputs.tf
@@ -5,3 +5,7 @@ output "cluster_role_arn" {
 output "node_role_arn" {
   value = aws_iam_role.eks_node_role.arn
 }
+output "dynamodb_policy_arn" {
+  value       = aws_iam_policy.dynamodb_access.arn
+  description = "ARN da policy de acesso ao DynamoDB"
+}
