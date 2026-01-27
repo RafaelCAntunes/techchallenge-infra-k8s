044ba5560fc8fb614f6ef34dcd07b661b4db8135
Rafael Antunes <rafaelcantunes3@gmail.com>
2026-01-27 01:17:53 -0300

    teste

--- providers.tf
+++ providers.tf
@@ -1,17 +1,17 @@
-provider "aws" {
+/*provider "aws" {
   region = var.aws_region
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = try(var.cluster_name,"")
+  name = var.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = try(var.cluster_name,"")
+  name = var.cluster_name
 }
 
 provider "kubernetes" {
   host                   = try(data.aws_eks_cluster.cluster[0].endpoint, null)
   cluster_ca_certificate = try(base64decode(data.aws_eks_cluster.cluster[0].certificate_authority[0].data), null)
   token                  = try(data.aws_eks_cluster_auth.cluster[0].token, null)
-}
+}*/
