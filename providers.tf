0c934582c03ed7e9c98573f7b6cf9c4fadb3a129
Rafael Antunes <rafaelcantunes3@gmail.com>
2026-01-27 00:59:53 -0300

    ajustando provider para não quebrar no plan do tf por ainda não ter o recurso.

--- providers.tf
+++ providers.tf
@@ -11,7 +11,7 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  host                   = try(data.aws_eks_cluster.cluster[0].endpoint, null)
+  cluster_ca_certificate = try(base64decode(data.aws_eks_cluster.cluster[0].certificate_authority[0].data), null)
+  token                  = try(data.aws_eks_cluster_auth.cluster[0].token, null)
 }
